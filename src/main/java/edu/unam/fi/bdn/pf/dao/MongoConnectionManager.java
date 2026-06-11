bb08ef7bfc27152ddc111181ed67f4d85b2b5601
ivancano <ivan.fernandez@ingenieria.unam.edu>
2026-06-11 11:06:05 -0600

    fear: test mongo connection and query

--- src/main/java/edu/unam/fi/bdn/pf/dao/MongoConnectionManager.java
+++ src/main/java/edu/unam/fi/bdn/pf/dao/MongoConnectionManager.java
@@ -1,4 +1,30 @@
 package edu.unam.fi.bdn.pf.dao;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class MongoConnectionManager {
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String CONNECTION_URI = dotenv.get("MONGO_URI");
+    private static final String DATABASE_NAME = dotenv.get("MONGO_DATABASE");
+    private static MongoClient mongoClient = null;
+
+    // El objeto MongoClient no representa una sola conexión física, sino que administra un pool de conexiones
+    public static MongoDatabase getDatabase() {
+        if (mongoClient == null) {
+            // solo se instancia un objeto por app, no varios
+            mongoClient = MongoClients.create(CONNECTION_URI);
+        }
+        return mongoClient.getDatabase(DATABASE_NAME);
+    }
+
+    // cierre controlado
+    public static void closeConnection() {
+        if (mongoClient != null) {
+            mongoClient.close();
+            mongoClient = null;
+        }
+    }
 }
--- src/test/java/edu/unam/fi/bdn/pf/MongoConnectionTest.java
+++ src/test/java/edu/unam/fi/bdn/pf/MongoConnectionTest.java
@@ -1,4 +1,53 @@
 package edu.unam.fi.bdn.pf;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import edu.unam.fi.bdn.pf.dao.MongoConnectionManager;
+import org.bson.Document;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class MongoConnectionTest {
+    private static MongoDatabase database;
+
+    @BeforeAll
+    public static void setUp() {
+        // se inicializa la conexión general antes de ejecutar los casos de prueba
+        database = MongoConnectionManager.getDatabase();
+    }
+
+    @Test
+    public void pingMongoDB() {
+        Document pingCommand = new Document("ping", 1);
+        Document response = database.runCommand(pingCommand);
+
+        // mongoDB responde con { "ok": 1.0 } si el ping fue exitoso
+        assertNotNull(response, "La respuesta del comando ping es nula");
+        assertEquals(1.0, response.getDouble("ok"),
+                "Conexión no exitosa hacia mongodb");
+    }
+
+    @Test
+    public void testArtistExists() {
+        // recuperamos cualquier conexión, en este caso artists
+        MongoCollection<Document> artistsCollection = database.getCollection("artists");
+
+        // ejecutamos la consulta
+        Document query = new Document("name", "The Beatles");
+        Document result = artistsCollection.find(query).first();
+
+        assertNotNull(result, "Documento no encontrado");
+        assertEquals("The Beatles", result.getString("name"),
+                "No se encontró al artista");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        MongoConnectionManager.closeConnection();
+    }
 }
+
